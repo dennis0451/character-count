678d05921c0bf9b8d50615120c5f20ef121a6860
Dennis Corral <dennis.corral@outlook.com>
2022-05-28 12:21:00 -0400

    working solution

--- js/charCount.js
+++ js/charCount.js
@@ -1,12 +1,21 @@
 exports.charCount = function(string) {
-    //sort array and return sorted array
+    //turning string into object of occurences
     let charObj = {}
     for(let i = 0; i < string.length ; i++){
-        if(charObj.hasOwnProperty(string[i])){
-            charObj[string[i]] += 1
-        }else{
-            charObj[string[i]] = 1
+        if(string[i] != " "){
+            charObj.hasOwnProperty(string[i])? charObj[string[i]]++ : charObj[string[i]] = 1
         }
     }
-    return Object.entries(charObj)
+    //turn object into an array
+    let charArray = Object.entries(charObj)
+    //sort array and return sorted array
+    let result = charArray.sort(function (a,b){
+        if (a[1] === b[1] ){
+            if (a < b){return -1}
+        }
+         if (a[1] < b[1] ) {return 1}
+         if (a[1] > b[1] ) {return -1}
+    }
+    )
+return result    
 };
