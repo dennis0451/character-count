d836dac021269708116eedbad602ee2dff4dec4f
Dennis Corral <dennis.corral@outlook.com>
2022-05-27 22:03:09 -0400

    mid solution

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,12 @@
-exports.charCount = function() {
-
+exports.charCount = function(string) {
+    //sort array and return sorted array
+    let charObj = {}
+    for(let i = 0; i < string.length ; i++){
+        if(charObj.hasOwnProperty(string[i])){
+            charObj[string[i]] += 1
+        }else{
+            charObj[string[i]] = 1
+        }
+    }
+    return Object.entries(charObj)
 };
