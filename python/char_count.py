a08c2d2e91a88761c9c4dd7d573e31e9e9daa5c0
Dennis Corral <dennis.corral@outlook.com>
2022-05-31 08:53:56 -0400

    working solution

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,21 @@
+from functools import cmp_to_key
+
 def char_count(str):
-  # Your code here
+  chars ={}
+  for char in str:
+    if char != " ":
+      if char in chars:
+        chars[char] += 1
+      else:
+        chars[char] = 1
+  # //turn char dict to list
+  list_of_chars = []
+  for key,val in chars.items():
+    list_of_chars.append([key]+ [val])
+    print(sorted(list_of_chars, key=lambda x : (-x[1], x[0])))
+  return sorted(list_of_chars, key=lambda x : (-x[1], x[0]))
+  # print(list_of_chars)
+
+# print(char_count("an apple a day will keep the doctor away"))
+
+
